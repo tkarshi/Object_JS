b5a36bb6382ab859140495f97c590032cafb1681
tkarshi <tkarshi2000@gmail.com>
2024-07-13 11:03:43 +0530

    delete property and add property

--- add_property.js
+++ add_property.js
@@ -3,6 +3,7 @@ let day1 = {
   events: ["Music", "Dance", "Art", "Drama"],
 };
 
+//add property
 day1.time = "10 am";
 
 console.log(day1.time);
@@ -11,6 +12,23 @@ console.log(day1.time);
 
 let test = {
   "Unit Testing": "approved",
+  time: "10 am",
+  out: "3 pm",
 };
 
 console.log(test["Unit Testing"]);
+
+//Delete property
+delete test["time"];
+
+console.log(test);
+
+//add property
+test.in = "come in";
+
+console.log(test);
+
+//delete property
+delete test["Unit Testing"];
+
+console.log(test);
